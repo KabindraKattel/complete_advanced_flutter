16e7f5d0e28d53e77bad0c44d7e3b8c1c5af3f3b
KabindraKattel <kattelk4b1n@gmail.com>
2021-11-23 22:48:46 +0545

    upto Style Manager

--- lib/presentation/styles_manager.dart
+++ lib/presentation/styles_manager.dart
@@ -14,48 +14,53 @@ TextStyle _getTextStyle(
 ///Light TextStyle
 TextStyle getLightStyle(
     {double fontSize = FontSize.s12, required Color color}) {
-  return TextStyle(
-      fontFamily: FontConstants.fontFamily,
-      fontWeight: FontWeightManager.light,
-      fontSize: fontSize,
-      color: color);
+  return _getTextStyle(
+    FontConstants.fontFamily,
+    FontWeightManager.light,
+    fontSize,
+    color,
+  );
 }
 
 ///Regular TextStyle
 TextStyle getRegularStyle(
     {double fontSize = FontSize.s12, required Color color}) {
-  return TextStyle(
-      fontFamily: FontConstants.fontFamily,
-      fontWeight: FontWeightManager.regular,
-      fontSize: fontSize,
-      color: color);
+  return _getTextStyle(
+    FontConstants.fontFamily,
+    FontWeightManager.regular,
+    fontSize,
+    color,
+  );
 }
 
 ///Medium TextStyle
 TextStyle getMediumStyle(
     {double fontSize = FontSize.s12, required Color color}) {
-  return TextStyle(
-      fontFamily: FontConstants.fontFamily,
-      fontWeight: FontWeightManager.medium,
-      fontSize: fontSize,
-      color: color);
+  return _getTextStyle(
+    FontConstants.fontFamily,
+    FontWeightManager.medium,
+    fontSize,
+    color,
+  );
 }
 
 ///SemiBold TextStyle
 TextStyle getSemiBoldStyle(
     {double fontSize = FontSize.s12, required Color color}) {
-  return TextStyle(
-      fontFamily: FontConstants.fontFamily,
-      fontWeight: FontWeightManager.semiBold,
-      fontSize: fontSize,
-      color: color);
+  return _getTextStyle(
+    FontConstants.fontFamily,
+    FontWeightManager.semiBold,
+    fontSize,
+    color,
+  );
 }
 
 ///Bold TextStyle
 TextStyle getBoldStyle({double fontSize = FontSize.s12, required Color color}) {
-  return TextStyle(
-      fontFamily: FontConstants.fontFamily,
-      fontWeight: FontWeightManager.bold,
-      fontSize: fontSize,
-      color: color);
+  return _getTextStyle(
+    FontConstants.fontFamily,
+    FontWeightManager.bold,
+    fontSize,
+    color,
+  );
 }
